98cfa31c36d57f5710a1591f30ba4c7046503b61
Alura Verde <aluraverde@Alura-Verde.home>
2019-06-25 21:01:20 -0300

    adicionando slack

--- notificacao.sh
+++ notificacao.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"'$1'"}' https://hooks.slack.com/services/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
+if [ $1 == 'sucesso' ]
+then
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"deu tudo certo na pipeline!"}' https://hooks.slack.com/services/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
+else
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"algo deu errado na pipeline"}' https://hooks.slack.com/services/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
+fi
